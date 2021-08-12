8831fc8e93c669e2694f636a53613280c78f62a0
Moschops <evolaric@gmail.com>
2021-08-12 04:00:17 -0700

    base file cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const port = 3000;
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_req: Request, res: Response): void => {
   res.send(`<h2>Api is up and running on port: ${port}</h2>`);
 });
 
-app.listen(port, () => {
+app.listen(port, (): void => {
   console.log(`App listening on port ${port}`);
 });
 
